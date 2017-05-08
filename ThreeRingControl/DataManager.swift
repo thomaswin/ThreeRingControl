59a4a759b377c8e04a14759da45532982df1520f
Tun Lin <tunlin@nets.com.sg>
2017-05-08 16:11:48 +0800

    Update data manager

---
+++ ThreeRingControl/DataManager.swift
@@ -0,0 +1,17 @@
+//
+//  DataManager.swift
+//  ThreeRingControl
+//
+//  Created by Innovation on 5/8/17.
+//  Copyright © 2017 NETS. All rights reserved.
+//
+
+import Foundation
+import BoltsSwift
+
+public class DataManager {
+    
+    public func doSomething(name : String) {
+        print ("Hello : \(name)")
+    }
+}
